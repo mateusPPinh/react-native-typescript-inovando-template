d6f8d12758379741be585853b1896d918734b943
matheuspinheiroP <rawrente@gmail.com>
2021-03-14 17:41:27 -0300

    chore: include button and input components

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -2,38 +2,29 @@ import React from 'react';
 
 import {
   Container,
-  FormContainer,
   Logo,
   Form,
-  Input,
   ForgotPasswordButtton,
   ForgotPasswordButtonText,
-  LoginButton,
-  LoginButtonText,
 } from './styles';
 import inovandoLogo from '../../assets/inovando-logo-full.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Signin: React.FC = () => (
   <Container>
-    <FormContainer>
-      <Logo source={inovandoLogo} />
+    <Logo source={inovandoLogo} />
 
-      <Form>
-        <Input placeholder="Seu melhor e-mail" />
-        <Input placeholder="Seu cpf" />
-        <Input placeholder="Sua senha segura" />
+    <Form>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" secureTextEntry />
 
-        <ForgotPasswordButtton>
-          <ForgotPasswordButtonText>
-            Esqueci minha senha
-          </ForgotPasswordButtonText>
-        </ForgotPasswordButtton>
+      <Button>Entrar</Button>
 
-        <LoginButton>
-          <LoginButtonText>Logar</LoginButtonText>
-        </LoginButton>
-      </Form>
-    </FormContainer>
+      <ForgotPasswordButtton>
+        <ForgotPasswordButtonText>Esqueci minha senha</ForgotPasswordButtonText>
+      </ForgotPasswordButtton>
+    </Form>
   </Container>
 );
 
