3534177062ab93b9db0e4956669ae860eb18d7dd
matheuspinheiroP <rawrente@gmail.com>
2021-03-21 15:25:58 -0300

    fix: increment more configs

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
 
 const baseURL = '';
-
-`${Platform.OS === 'android' ? 'localhost' : '10.0.2.2'}`;
 
 const api = axios.create({
   baseURL,
