35ee762e1d211b406339d5aae54dde5768f6d0ad
matheuspinheiroP <rawrente@gmail.com>
2021-03-21 10:49:46 -0300

    fix: increment unform configs

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React, { useCallback, useRef } from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
 import {
   Container,
   Logo,
-  Form,
+  FormContainer,
   ForgotPasswordButtton,
   ForgotPasswordButtonText,
 } from './styles';
@@ -12,33 +14,50 @@ import inovandoLogo from '../../assets/inovando-logo-full.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const Signin: React.FC = () => (
-  <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-  >
-    <Container>
-      <Logo source={inovandoLogo} />
+const Signin: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <Form>
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon="lock"
-          placeholder="Senha"
-          secureTextEntry
-        />
+  const handleSign = useCallback((data: unknown) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }, []);
 
-        <Button>Entrar</Button>
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <Container>
+        <Logo source={inovandoLogo} />
 
-        <ForgotPasswordButtton>
-          <ForgotPasswordButtonText>
-            Esqueci minha senha
-          </ForgotPasswordButtonText>
-        </ForgotPasswordButtton>
-      </Form>
-    </Container>
-  </KeyboardAvoidingView>
-);
+        <FormContainer>
+          <Form ref={formRef} onSubmit={handleSign}>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+              secureTextEntry
+            />
+
+            <Button
+              onPress={() => {
+                formRef.current?.submitForm();
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPasswordButtton>
+              <ForgotPasswordButtonText>
+                Esqueci minha senha
+              </ForgotPasswordButtonText>
+            </ForgotPasswordButtton>
+          </Form>
+        </FormContainer>
+      </Container>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default Signin;
